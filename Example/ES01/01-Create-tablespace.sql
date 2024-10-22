af4f3a89b8e954e623c8e1e58a09dcd639e7cdb2
aminelli <56305052+aminelli@users.noreply.github.com>
2024-10-22 17:23:48 +0200

    upd

---
+++ Example/ES01/01-Create-tablespace.sql
@@ -0,0 +1,8 @@
+----------------------------------------------------
+-- Create Tablespace TS_CORSO
+----------------------------------------------------
+CREATE TABLESPACE TS_CORSO 
+   DATAFILE 'tbs_corso.dbf' 
+   SIZE 500M;
+
+COMMIT;
